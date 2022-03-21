d0c4280137970eafcb1c8854ba3ea30c4817763f
VicenzoColorado <102033902+VicenzoColorado@users.noreply.github.com>
2022-03-21 09:24:29 -0300

    Add files via upload

---
+++ questao1_1.js
@@ -0,0 +1,10 @@
+var aluno1 = "Vicenzo Casarotto"
+var aluno2 = "Ana Vitória Ribeiro Wundervald"
+var aluno3 = "Gabriel Rocha Gouveia"
+var aluno4 = "Kauã Henrique Martins "
+var aluno5 = "Júlia Fontanive da Silva"
+var aluno6 = "Alana Klug Potolowsky"
+
+/* para verificar */
+
+console.log(aluno1, aluno2, aluno3, aluno4, aluno5, aluno6)
